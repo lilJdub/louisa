04023f745a43b0a5bf24d6c6927798b1e828e10a
lilJdub <45808468+lilJdub@users.noreply.github.com>
2021-01-07 00:47:52 +0800

    Add files via upload
    Ver4.

---
+++ 顧客個人分析.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>顧客個人分析</title>
+	<style type="text/css">
+		*{
+			background-color: #FFC78E;
+		}
+		.top{
+			background-color: #FF9224;
+			text-align: center;
+		}
+		.txtarea{
+			background-color:white;
+			height:600px;
+			margin: 20px;
+		}
+	</style>
+</head>
+<body>
+	<div class="top">
+		顧客個人分析	
+	</div>
+	<div class="txtarea" >
+		
+	</div>
+</body>
+</html>
